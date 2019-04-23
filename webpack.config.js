90c030c30204861c2206f966aec5bb6f158a79e9
Roman <soht90@gmail.com>
2019-04-23 13:00:31 +0300

    created webpack, installed plugins

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+// webpack v4
+const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+            pretty: true
+        }
+      
+    },
+    {
+      test: /\.(eot|svg|ttf|woff|woff2)$/,
+      use: [
+               {
+                   loader: 'file-loader?name=./fonts/[name]/[name].[ext]'
+               }
+               
+           ]
+    },
+    {
+      test : /\.react\.js$/,
+      loader: 'webpack-bem-loader',
+      options: {
+          naming: 'react',
+          levels: ['./pathToBlocks'],
+          // OR:
+          // levels: {
+          //     './pathToBlocks': {
+          //         default: true,
+          //         scheme: 'nested',
+          //         naming: 'origin'
+          //     }
+          // },
+          techs: ['js', 'css'],
+          techMap: {
+              js : ['react.js']
+          },
+          langs: ['ru', 'en']
+      }
+  }
+    ]
+  },
+  plugins: [
+    // new ExtractTextPlugin(
+    //   {filename: 'style.[hash].css', disable: false, allChunks: true }
+    // ),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/index.pug'
+    })
+    //new WebpackMd5Hash()
+  ]
+};
