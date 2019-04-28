ecc5eb655eb540c107aed4191bfb7d3cd0148c7b
Roman <soht90@gmail.com>
2019-04-28 22:57:42 +0300

    created textfield, guests dropdown, starting make datepicker

--- dist/main.js
+++ dist/main.js
@@ -127,7 +127,7 @@ eval("\n/**\n * When source maps are enabled, `style-loader` uses a link element
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/main.scss */ \"./src/scss/main.scss\");\n/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_main_scss__WEBPACK_IMPORTED_MODULE_0__);\n\nconsole.log(\"hello, world\");\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/main.scss */ \"./src/scss/main.scss\");\n/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_main_scss__WEBPACK_IMPORTED_MODULE_0__);\n // import ncs from '../src/ncs.js'\n\nwindow.$ = jQuery;\nwindow.jQuery = jQuery;\nconsole.log(\"hello, world\"); // ncs()\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "./scss/main.scss";
+// import ncs from '../src/ncs.js'
+window.$ = jQuery;
+window.jQuery = jQuery;
 console.log("hello, world");
+// ncs()
