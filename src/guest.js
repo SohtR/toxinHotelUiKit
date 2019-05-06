c66e91d5bb174e2c3bb563adf9391fb718aaffd9
Roman <soht90@gmail.com>
2019-05-06 18:30:05 +0300

    made guests dropdown

---
+++ src/guest.js
@@ -0,0 +1,14 @@
+
+$("input[name='guest']").guest({
+    categoryNames: ["Взрослые", "Дети", "Младенцы"],
+    categoryValues: false,
+    minValue: 0,
+    maxValue: 10,
+    closeOnOutsideClick: false,
+    showText: true,
+    delimiter: ", ",
+    align: "left",
+    fade: true,
+    useDisplay: false,
+    showZero: false,
+});
