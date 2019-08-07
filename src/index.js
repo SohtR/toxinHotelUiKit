32f7ce0dc258d52641127d2b32830d2a9fa42b7d
Roman <soht90@gmail.com>
2019-08-07 18:38:00 +0300

    cleanup project folders, change img connection method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import "./scss/main.scss";
-import "./js/numbercategoryselector.js";
+import "./main.scss";
+import "./components/dropdownRoom/numbercategoryselector.js";
 import "./components/dropdownGuest/numbercategoryselectorGuest.js";
 import "./components/dropdownGuest2/numbercategoryselectorGuest2.js";
 import "./components/cardSliderRoom/jquery.flexslider.js";
 import "./components/cardSliderRoom/flexSlider.js";
 import "./components/rangeSlider/rangeSlider.js";
-import "./favicons/favicons.js";
-import "./js/datepicker.min.js";
+import "./img/favicons/favicons.js";
+
+import "./components/dataDropdown/datepicker.min.js";
 window.$ = jQuery;
 window.jQuery = jQuery;
