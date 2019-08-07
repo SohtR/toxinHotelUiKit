72b95e8799ecb04f02b509d7d9387538dd2229eb
Roman <soht90@gmail.com>
2019-08-07 12:34:19 +0300

    changed README.md ISSUE #1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# toxinHotel
+Demo Page: https://sohtr.github.io/toxinHotelUiPage/
+
+To clone repository to your local storage: git clone https://github.com/SohtR/toxinHotelUiKit.git
+Then you should install webpack: npm install webpack webpack-cli --save-dev
+
+You can use one of the three commands:
+
+    npm run dev - delete dist directory and compile project
+
+    npm run start - start local dev-server
+
+    npm run build - mode production
--- src/index.js
+++ src/index.js
@@ -5,13 +5,7 @@ import "./components/dropdownGuest2/numbercategoryselectorGuest2.js";
 import "./components/cardSliderRoom/jquery.flexslider.js";
 import "./components/cardSliderRoom/flexSlider.js";
 import "./components/rangeSlider/rangeSlider.js";
-
-
+import "./favicons/favicons.js";
 import "./js/datepicker.min.js";
 window.$ = jQuery;
 window.jQuery = jQuery;
-console.log("hello, world");
-// import "./guest2.js";
-// import "./guest.js";
-//import "./components/twoInputRange/twoInputRange.js";
-//import "./likeButton";
